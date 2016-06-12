96b64bf79446be46939e4cf06818657c20df6afa
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2016-06-12 11:03:53 +0200

    WifiScanning service is looped

---
+++ app/src/main/java/pl/ipepe/android/geowifi/ActivityWithSettings.java
@@ -0,0 +1,9 @@
+package pl.ipepe.android.geowifi;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by patrykptasinski on 12/06/16.
+ */
+public class ActivityWithSettings extends AppCompatActivity {
+}
