68b11c319562255976c7dce379f9d57a859dc469
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2016-08-27 20:51:19 +0200

    Update

--- app/src/main/java/pl/ipepe/android/geowifi/WifiObservation.java
+++ app/src/main/java/pl/ipepe/android/geowifi/WifiObservation.java
@@ -1,46 +1,57 @@
 package pl.ipepe.android.geowifi;
 
+import android.annotation.TargetApi;
 import android.net.wifi.ScanResult;
+import android.os.Build;
+import android.os.SystemClock;
+
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 
+import java.util.Date;
+
 /**
  * Created by patrykptasinski on 12/06/16.
  */
-@Table(name = "WifiObservations")
+@Table(name = "wifi_observations")
 public class WifiObservation extends Model {
 
-    @Column(name = "SSID")
-    public String SSID;
+    @Column(name = "ssid", index=true)
+    public String ssid;
 
-    @Column(name = "BSSID")
-    public String BSSID;
+    @Column(name = "bssid", index=true)
+    public String bssid;
 
-    @Column(name = "RSSID_LEVEL")
-    public int RSSID_LEVEL;
+    @Column(name = "signal_level")
+    public int signal_level;
 
-    public WifiObservation(){
+    @Column(name = "capabilities")
+    public String capabilities;
+
+    @Column(name = "seen_at")
+    public Date seen_at;
+
+    @Column(name = "channel_frequency")
+    public int channel_frequency;
+
+    public WifiObservation() {
         super();
-    }
-
-    public WifiObservation(String SSID, String BSSID, int RSSID_LEVEL){
-        super();
-        this.SSID = SSID;
-        this.BSSID = BSSID;
-        this.RSSID_LEVEL = RSSID_LEVEL;
     }
 
     public WifiObservation(ScanResult scanResult){
         super();
-        this.SSID = scanResult.SSID;
-        this.BSSID = scanResult.BSSID;
-        this.RSSID_LEVEL = scanResult.level;
+        this.ssid = scanResult.SSID;
+        this.bssid = scanResult.BSSID;
+        this.signal_level = scanResult.level;
+        this.capabilities = scanResult.capabilities;
+        this.seen_at = new Date();
+        this.channel_frequency = scanResult.frequency;
     }
 
     @Override
     public String toString(){
-        return String.format("WO: %d %s %s", this.RSSID_LEVEL, this.SSID, this.BSSID);
+        return String.format("WO: %d %s %s", this.signal_level, this.ssid, this.bssid);
     }
 }
 
